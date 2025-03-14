b518939b545ec88463cedea1adfac8669dfad66f
Maureen <119786119+mutindamaureen@users.noreply.github.com>
2025-03-14 16:05:17 +0300

    Update urls.py

--- attachment_management/urls.py
+++ attachment_management/urls.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("Django success")
+
 
 urlpatterns = [
+    path('', home_view, name='home'),  # Add the root URL mapping
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('activities.urls')),
